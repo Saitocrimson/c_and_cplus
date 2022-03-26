d019edf49f919c7eb3f040240775ed5e039141d3
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-03-26 20:11:15 -0200

    Update casa.cpp

--- glut/casa.cpp
+++ glut/casa.cpp
@@ -31,6 +31,89 @@ void circulo()
     glPopMatrix();
     glFlush();
 }
+void parede()
+{
+    glPushMatrix();
+    glLoadIdentity();
+    glTranslatef(-0.150f, 0.10f, 0.0f);
+    glColor4ub(255,255,255,255);
+    glBegin(GL_QUADS);
+        glVertex3f(-0.3f, 0.3f, 0.0f);                          // Top Left
+        glVertex3f( 0.3f, 0.3f, 0.0f);                          // Top Right
+        glVertex3f( 0.3f,-0.3f, 0.0f);                          // Bottom Right
+        glVertex3f(-0.3f,-0.3f, 0.0f);
+    glEnd();
+    glPopMatrix();
+    glFlush();
+
+}
+void porta()
+{
+    glPushMatrix();
+    glLoadIdentity();
+    glTranslatef(-0.150f, 0.10f, 0.0f);
+    glColor4ub(255,150,220,255);
+    glBegin(GL_QUADS);
+        glVertex3f(-0.1f, 0.1f, 0.0f);                          // Top Left
+        glVertex3f( 0.1f, 0.1f, 0.0f);                          // Top Right
+        glVertex3f( 0.1f,-0.3f, 0.0f);                          // Bottom Right
+        glVertex3f(-0.1f,-0.3f, 0.0f);
+    glEnd();
+    glPopMatrix();
+    glFlush();
+
+}
+void tronco()
+{
+    glPushMatrix();
+    glLoadIdentity();
+    glTranslatef(0.700f, 0.10f, 0.0f);
+    glColor4ub(150,28,0,255);
+    glBegin(GL_QUADS);
+         glVertex3f(-0.1f, 0.1f, 0.0f);                          // Top Left
+         glVertex3f( 0.1f, 0.1f, 0.0f);                          // Top Right
+         glVertex3f( 0.1f,-0.3f, 0.0f);                          // Bottom Right
+          glVertex3f(-0.1f,-0.3f, 0.0f);
+    glEnd();
+    glPopMatrix();
+    glFlush();
+
+
+}
+void chao()
+{
+    glPushMatrix();
+    glLoadIdentity();
+    glTranslatef(-0.150f, -0.50f, 0.0f);
+    glColor4ub(0,255,0,255);
+    glBegin(GL_QUADS);
+          glVertex3f(-1.9f, 0.3f, 0.0f);                          // Top Left
+          glVertex3f( 1.9f, 0.3f, 0.0f);                          // Top Right
+          glVertex3f( 1.9f,-0.6f, 0.0f);                          // Bottom Right
+          glVertex3f(-1.9f,-0.6f, 0.0f);
+    glEnd();
+    glPopMatrix();
+    glFlush();
+}
+
+void telhado()
+{
+    glPushMatrix();
+    glLoadIdentity();
+    glTranslatef(-0.150f, 0.50f, 0.0f);
+    glRotatef(300.0,160.0,1.0,0.0);
+    glScalef(1.0,1.0,1.0);
+    glBegin(GL_TRIANGLES);
+         glColor3f(1.00,0.00,0.00);
+         glVertex2f(-0.5,-0.5);
+         glColor3f(1.0,0.0,0.0);
+         glVertex2f(0.5,-0.5);
+         glColor3f(1.0,0.0,0.0);
+         glVertex2f(0.0,0.5);
+    glPopMatrix();
+    glEnd();
+    glFlush();
+}
 
 
 void inicia()
@@ -43,80 +126,22 @@ glClearColor(0.0f,0.8f,1.0f,1.0f);
 void desenha()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+//parede da casa 
+    parede();
+//porta
+    porta();
 
-    glPushMatrix();
-    glLoadIdentity();
-    glTranslatef(-0.150f, 0.10f, 0.0f);
-    glColor4ub(255,255,255,255);
-//parede da casa
-    glBegin(GL_QUADS);
-    glVertex3f(-0.3f, 0.3f, 0.0f);                          // Top Left
-    glVertex3f( 0.3f, 0.3f, 0.0f);                          // Top Right
-    glVertex3f( 0.3f,-0.3f, 0.0f);                          // Bottom Right
-    glVertex3f(-0.3f,-0.3f, 0.0f);
-    glEnd();
-    glPopMatrix();
-    glFlush();
-    //porta
-    glPushMatrix();
-    glLoadIdentity();
-    glTranslatef(-0.150f, 0.10f, 0.0f);
-    glColor4ub(255,150,220,255);
-    glBegin(GL_QUADS);
-    glVertex3f(-0.1f, 0.1f, 0.0f);                          // Top Left
-    glVertex3f( 0.1f, 0.1f, 0.0f);                          // Top Right
-    glVertex3f( 0.1f,-0.3f, 0.0f);                          // Bottom Right
-    glVertex3f(-0.1f,-0.3f, 0.0f);
-    glEnd();
-    glPopMatrix();
-    glFlush();
-
-    //tronco
-    glPushMatrix();
-    glLoadIdentity();
-    glTranslatef(0.700f, 0.10f, 0.0f);
-    glColor4ub(150,28,0,255);
-    glBegin(GL_QUADS);
-    glVertex3f(-0.1f, 0.1f, 0.0f);                          // Top Left
-    glVertex3f( 0.1f, 0.1f, 0.0f);                          // Top Right
-    glVertex3f( 0.1f,-0.3f, 0.0f);                          // Bottom Right
-    glVertex3f(-0.1f,-0.3f, 0.0f);
-    glEnd();
-    glPopMatrix();
-    glFlush();
- //copa da arvore
+//tronco
+    tronco();
+ 
+//copa da arvore
     circulo();
- //chao
-    glPushMatrix();
-    glLoadIdentity();
-    glTranslatef(-0.150f, -0.50f, 0.0f);
-    glColor4ub(0,255,0,255);
-    glBegin(GL_QUADS);
-    glVertex3f(-1.9f, 0.3f, 0.0f);                          // Top Left
-    glVertex3f( 1.9f, 0.3f, 0.0f);                          // Top Right
-    glVertex3f( 1.9f,-0.6f, 0.0f);                          // Bottom Right
-    glVertex3f(-1.9f,-0.6f, 0.0f);
-    glEnd();
-    glPopMatrix();
-    glFlush();
 
- //telhado
+//chao
+    chao();
 
-    glPushMatrix();
-    glLoadIdentity();
-    glTranslatef(-0.150f, 0.50f, 0.0f);
-    glRotatef(300.0,160.0,1.0,0.0);
-    glScalef(1.0,1.0,1.0);
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.00,0.00,0.00);
-    glVertex2f(-0.5,-0.5);
-    glColor3f(1.0,0.0,0.0);
-    glVertex2f(0.5,-0.5);
-    glColor3f(1.0,0.0,0.0);
-    glVertex2f(0.0,0.5);
-    glPopMatrix();
-    glEnd();
-    glFlush();
+//telhado
+    telhado();
 
 }
 
@@ -127,7 +152,7 @@ int main(int argc, char** argv)
     glutInitWindowSize(640,480);
     glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutCreateWindow("odio");
+    glutCreateWindow("casa");
     inicia();
     glutDisplayFunc(desenha);
     glutMainLoop();
