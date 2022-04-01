c1c17c3676a267018f564344fee7ea0333de4d06
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-04-01 18:22:59 -0300

    Update casa.cpp

--- glut/casa.cpp
+++ glut/casa.cpp
@@ -1,10 +1,13 @@
 #include<Windows.h>
 #include <GL/gl.h>
+#include <GL/glut.h>
 #include <GL/freeglut.h>
 #include <stdlib.h>
 #include <math.h>
 #include <cmath>
 #define M_PI       3.14159265358979323846
+
+
 
 
 void circulo()
@@ -67,13 +70,13 @@ void tronco()
 {
     glPushMatrix();
     glLoadIdentity();
-    glTranslatef(0.700f, 0.10f, 0.0f);
+    glTranslatef(0.650f, 0.10f, 0.0f);
     glColor4ub(150,28,0,255);
     glBegin(GL_QUADS);
-         glVertex3f(-0.1f, 0.1f, 0.0f);                          // Top Left
+         glVertex3f(0.0f, 0.1f, 0.0f);                          // Top Left
          glVertex3f( 0.1f, 0.1f, 0.0f);                          // Top Right
          glVertex3f( 0.1f,-0.3f, 0.0f);                          // Bottom Right
-          glVertex3f(-0.1f,-0.3f, 0.0f);
+          glVertex3f(0.0f,-0.3f, 0.0f);
     glEnd();
     glPopMatrix();
     glFlush();
@@ -110,31 +113,47 @@ void telhado()
          glVertex2f(0.5,-0.5);
          glColor3f(1.0,0.0,0.0);
          glVertex2f(0.0,0.5);
-    glPopMatrix();
+
     glEnd();
+    glPopMatrix();
     glFlush();
 }
 
+void macaneta()
+{
+	 glLoadIdentity();
+     glTranslatef(-0.320f, -0.05f, 0.0f);
+     glLineWidth(20);
+     glColor3f(0.0,0.0,0.0);
+     glBegin(GL_LINES);
+          glVertex2f(0.1,0.0);  // primeiro vértice
+          glVertex2f(0.1,0.1); // segundo vértice
+     glEnd();
+     glFlush();
+}
 
 void inicia()
 {
 
 glClearColor(0.0f,0.8f,1.0f,1.0f);
 
+
 }
 
 void desenha()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-//parede da casa 
+
+//parede da casa
     parede();
 //porta
     porta();
 
 //tronco
     tronco();
- 
+
 //copa da arvore
+
     circulo();
 
 //chao
@@ -142,19 +161,21 @@ void desenha()
 
 //telhado
     telhado();
+    //ceu*/
 
+    macaneta();
 }
 
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
+    glutInitWindowSize(800,640);
     glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutCreateWindow("casa");
-    inicia();
     glutDisplayFunc(desenha);
+    inicia();
     glutMainLoop();
     return 0;
 
