1c86dc5644d0b912ad82b8b066618d864d944d75
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-08-22 15:29:19 -0300

    Create ex08.c

---
+++ exercicio_de_SO2/ex08.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+#define pi 3.14
+
+
+void esfera(double a, double *b)
+{
+    double v;
+    v=(4* pi *(pow(a,3)))/3;
+ //   (*b)=(*b)+12; teste para ponteiro
+   
+    printf("O reultADO eh: %lf\n", v);
+}
+
+
+void le(double *x)
+{
+  printf ("Digite o raio\n");
+  scanf ("%lf", &(*x));
+
+    
+}
+
+int main ()
+{
+  int a, b, c;
+  double r=0;
+   le(&r);
+   esfera(r,&r);
+  
+  // printf("%lf\n", r);//esse teste serve apenas para relembrar ponteiro 
+   return 0;
+}
