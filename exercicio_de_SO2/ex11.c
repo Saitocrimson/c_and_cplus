b809ec1ec62471734fd473d737dab1b4c9a5afb3
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-08-23 12:20:30 -0300

    Create ex11.c

---
+++ exercicio_de_SO2/ex11.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int fatorial(int n, int soma){
+    if(n==0) return soma;
+    soma=soma*n;
+    if(n>0)return fatorial((n-1),soma);
+} 
+
+void ler(int *n)
+{
+    printf("Digite um numero: ");
+    scanf("%d", &(*n));
+}
+
+void operacao(int n)
+{
+    int soma=1;
+    ler(&n);
+    printf("o fatorial de %d eh %d\n",n, fatorial(n,soma));
+}
+
+int main()
+{
+   int n;
+   operacao(n);
+   return 0;
+}
