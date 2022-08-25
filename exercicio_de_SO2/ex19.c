53e581e1ba73fede2de6b4a9003f33d1196ae1ce
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-08-24 22:12:44 -0200

    Create ex19.c

---
+++ exercicio_de_SO2/ex19.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+void compara(double cops[], double temp[], char mes[][12]){
+for(int i=0;i<12;i++)
+{
+if(temp[i]==cops[0])printf("Menor temperatura eh %.2lf do mes %s\n", temp[i], mes[i]);
+  if(temp[i]==cops[11])printf("Maior temperatura eh %.2lf do mes %s\n", temp[i], mes[i]);
+}
+}
+
+
+
+
+void arruma(double cops[])
+{
+  double copia;
+  for(int i=0;i<12;i++){
+    for(int j=0; j<11; j++){
+      if(cops[j]>cops[j+1])
+      {
+        copia=cops[j];
+        cops[j]=cops[j+1];
+        cops[j+1]=copia;
+
+
+
+
+
+      }
+    }
+  }
+
+  }
+void ler(char meses[][12], double temp[],double cops []){
+  for(int i=0; i<12;i++){
+printf("Digite o mes\n");
+scanf("%s", meses[i]);
+printf("Digite a temperatura\n");
+  scanf("%lf", &temp[i]);
+cops[i]=temp[i];
+
+
+}
+}
+int main() {
+    char meses[12][12];
+    double temp[12], cops[12];
+    ler(meses, temp,cops);
+    arruma(cops);
+    compara(cops,temp,meses);
+    return 0;
+}
+  
+  
