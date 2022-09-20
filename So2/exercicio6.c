795b905fa4baa0f733dce5ba6e6ed78b6a9d2c71
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-09-20 17:23:59 -0300

    Update exercicio6.c

--- So2/exercicio6.c
+++ So2/exercicio6.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-#include<unistd.h>
-int v[1000];
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
 
-
-
-int somavetor(int id)
+int somaPares(int id, int v[1000])
 {
     int i,soma=0;
     int inicio = id * 250;
@@ -18,27 +18,25 @@ int somavetor(int id)
 
 int main()
 {
-        int x,y=0;
-        for(int i=0;i<1000;i++){
+        int x,*y,i,v[1000];
+        int shmid = shmget(IPC_PRIVATE, 100, 0600);
+         y = shmat(shmid, 0, 0);
+        *y=0;
+     for(int i=0;i<1000;i++)
                 v[i] = i;
-
-}
-        x=fork();
-        y=y+somavetor(0);
-        if(x!=0){
-            x=fork();
-            y=y+somavetor(1);
-        }
-    
-    if(x!=0){
-            x=fork();
-            y=y+somavetor(2);
-        }if(x!=0)
-        {
-       y=y+somavetor(3);
-        }
-        printf("Total %d\n", y);
-       // printf("id=%d\n", getpid());
+     for(i = 0; i < 3; i++){
+      x = fork();
+      if(x == 0){
+        *y+=somaPares(i, v);
+        exit(0);
+      }
+    }
+      
+     *y+=somaPares(i,v);
+     for(i=0; i<3; i++) wait(NULL);
+    printf("Soma Total = %d\n", *y);
+    shmdt(y);
+    shmctl(shmid, IPC_RMID, 0);
+    return 0;   
        
-        while(1);
 }
