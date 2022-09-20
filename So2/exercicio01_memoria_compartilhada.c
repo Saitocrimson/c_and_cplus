383a4f0e3d035c09cd5529ef0b72d48dbc4505c6
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-09-20 11:13:35 -0200

    Update exercicio01_memoria_compartilhada.c

--- So2/exercicio01_memoria_compartilhada.c
+++ So2/exercicio01_memoria_compartilhada.c
@@ -1,29 +1,33 @@
+
+
 #include <stdio.h> 
 #include <unistd.h> 
 #include <stdlib.h> 
 #include <sys/wait.h> 
 #include <sys/shm.h> 
+#include <string.h>
+
 
 
 
 int main() { 
-  int x=1;
-  char *teste[10]; 
-  int shmid = shmget(IPC_PRIVATE, 100, 0600); 
-  teste[0]= (char *)shmat(shmid, (void *)0, 0); 
-  printf("Pai pede pra  dizer algo\n");
-  x = fork(); 
-  if(x == 0){ 
-printf("Filho disse: ");
-
-scanf("%s", *teste); 
+   int x=1;
+   char *teste[100], *pudim[10];
+   int shmid = shmget(IPC_PRIVATE, 100, 0600); 
+   teste[0]= (char *)shmat(shmid, (void *)0, 0); 
+   strcpy(*teste,"socorro");
+   printf("Pai %s pede pra  dizer algo\n",*teste);
+   x = fork(); 
+   if(x == 0){ 
+       printf("Filho disse: ");
+       scanf("%s", *teste); 
 }  
     
 
-wait(NULL);
-if(x!=0) printf("E o pai repetiu:%s ???\n",*teste); 
-shmdt(teste); 
+   wait(NULL);
+   if(x!=0) printf("E o pai repetiu:%s ???\n",*teste); 
+   shmdt(teste); 
   
   //printf("Meu id=%d\n", getpid());
-shmctl(shmid, IPC_RMID, 0);
- return 0; }
+   shmctl(shmid, IPC_RMID, 0);
+   return 0; }
