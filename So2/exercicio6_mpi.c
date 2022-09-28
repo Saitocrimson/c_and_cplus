6e61fe4b0095c91e0d64b76f2faa80686032eda7
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2022-09-28 15:23:36 -0300

    Update exercicio6_mpi.c

--- So2/exercicio6_mpi.c
+++ So2/exercicio6_mpi.c
@@ -30,8 +30,9 @@ int main(int argc, char *argv[])
   if(id==0){
 
               soma+=somar(id,v);
+              
               printf("id 0-%d\n", soma);
-              MPI_Send(&soma,1,MPI_INT,1,0,MPI_COMM_WORLD);
+              MPI_Send(&soma,1,MPI_INT,3,0,MPI_COMM_WORLD);
 
      }
 
@@ -39,9 +40,8 @@ int main(int argc, char *argv[])
 
               soma+=somar(id,v);
               printf("id 1-%d\n", soma);
-              MPI_Status st;
-              MPI_Recv(&soma,1,MPI_INT,0,0,MPI_COMM_WORLD, &st);
-              MPI_Send(&soma,1,MPI_INT,1,0,MPI_COMM_WORLD);
+             
+              MPI_Send(&soma,1,MPI_INT,3,0,MPI_COMM_WORLD);
 
 
      }
@@ -50,9 +50,8 @@ int main(int argc, char *argv[])
 
               soma+=somar(id,v);
               printf("id 2-%d\n", soma);
-              MPI_Status st;
-              MPI_Recv(&soma,1,MPI_INT,0,0,MPI_COMM_WORLD, &st);
-              MPI_Send(&soma,1,MPI_INT,1,0,MPI_COMM_WORLD);
+             
+              MPI_Send(&soma,1,MPI_INT,3,0,MPI_COMM_WORLD);
 
      }
 
@@ -60,18 +59,18 @@ int main(int argc, char *argv[])
 
   if(id==3) {
 
-          soma+=somar(id,v);
-          printf("id 3-%d\n", soma);
+          int t,o,p,s;
+     	  soma+=somar(id,v);
+     	  s=soma+t+o+p;
+          
           MPI_Status st;
-          MPI_Recv(&soma,1,MPI_INT,0,0,MPI_COMM_WORLD, &st);
-
+          MPI_Recv(&t,1,MPI_INT,0,0,MPI_COMM_WORLD, &st);
+          MPI_Recv(&o,1,MPI_INT,1,0,MPI_COMM_WORLD, &st);
+          MPI_Recv(&p,1,MPI_INT,2,0,MPI_COMM_WORLD, &st);
+	  printf("id 3-%d\n", s);
 
 
 
   }
   MPI_Finalize();
 }
-
-
-
-
